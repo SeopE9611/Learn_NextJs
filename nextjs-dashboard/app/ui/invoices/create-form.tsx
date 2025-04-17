cd5004aa186a2016f763d00059f0ff9cfbb1f681
baxteryhs1118 <baxteryhs1118@gmail.com>
2025-04-17 20:04:12 +0900

    폼 검증 개선: 금액 및 상태 필드에 대한 오류 메시지 표시 기능 추가

--- nextjs-dashboard/app/ui/invoices/create-form.tsx
+++ nextjs-dashboard/app/ui/invoices/create-form.tsx
@@ -70,8 +70,18 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby='amount-error'
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+            <div id = 'amount-error' aria-live='polite' aria-atomic='true'>
+              {state.errors?.amount && 
+                state.errors.amount.map((error: string) => ( // amount라고 해서 타입이 number가 아니라 string 이여야한다. 왜냐하면 actions파일에서 amount?: string[] 로 선언했기 때문에 콜백인자를 string으로 처리해야한다.
+                  <p className='mt-2 text-sm text-red-500' key={error}>
+                    {error}
+                  </p>
+                ))
+              }
             </div>
           </div>
         </div>
@@ -90,6 +100,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby='status-error'
                 />
                 <label
                   htmlFor="pending"
@@ -105,6 +116,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="paid"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby='status-error'
                 />
                 <label
                   htmlFor="paid"
@@ -115,6 +127,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               </div>
             </div>
           </div>
+          <div id= 'status-error' aria-live='polite' aria-atomic='true'>
+                {state.errors?.status && 
+                  state.errors.status.map((error: string) => (
+                    <p className='mt-2 text-sm text-red-500' key={error}>
+                      {error}
+                    </p>
+                  ))}
+              </div>
         </fieldset>
       </div>
       <div className="mt-6 flex justify-end gap-4">
